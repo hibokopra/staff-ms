ee030bcf20852a4bafc21e11e58f274a8f17535b
Hibo Abdiaziz <hibokopra80@gmail.com>
2025-05-02 14:51:24 +0300

    Add files via upload

---
+++ src/main/java/com/example/staffsystem_C1220642/StaffApplication.java
@@ -0,0 +1,11 @@
+package com.example.staffsystem_C1220642;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaffApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(StaffApplication.class, args);
+	}
+}
